70d376c494886c544ac562c404e1aa14e3b564f3
Todd W Crone <tcrone@newrelic.com>
2024-03-01 14:43:36 -0500

    Checkpoint

---
+++ example-scene.js
@@ -0,0 +1,12 @@
+import * as THREE from "three";
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+// create a scene
+
+// setup camera
+
+// setup the renderer and attach to canvas
+
+// add lights
+
+// create a cube and torus knot and add them to the scene
--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
+scene.backgroundColor = 0xffffff;
+scene.fog = new THREE.Fog(0xffffff, 0.0025, 50);
+scene.add(new THREE.AmbientLight(0x666666));
+const dirLight = new THREE.DirectionalLight(0xaaaaaaa);
+dirLight.position.set(5, 12, 8);
+dirLight.castShadow = true;
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.VSMShadowMap;
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0xffffff);
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -12,7 +25,12 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.x = -3;
+camera.position.z = 8;
+camera.position.y = 2;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
 
 function animate() {
     cube.rotation.x += 0.01;
